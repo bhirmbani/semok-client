1adce953adf9bcccbc6fb6648434b117298a14a3
Bhirawa Mbani <bhirmbani@gmail.com>
2017-07-25 15:08:26 +0700

    adding category and top category logic

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 const Category = require('../controllers/category');
 
+router.post('/item', Category.assignToItem);
 router.post('/', Category.create);
-router.post('/item', Category.assignCategoryToItem);
+router.post('/top', Category.createTop);
+router.post('/top/assign', Category.assignCatToTop);
+router.delete('/:categoryId', Category.delete);
+router.put('/:categoryId', Category.edit);
 router.get('/', Category.gets);
+router.get('/top', Category.getsTopCategory);
 
 module.exports = router;
