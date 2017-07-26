e5ac9858e9090138e0236fa802245e095d1f5e19
Bhirawa Mbani <bhirmbani@gmail.com>
2017-07-26 11:06:48 +0700

    add package for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Grid } from 'semantic-ui-react';
+
+import TableItem from './components/table-item';
+import Navbar from './components/navbar';
+
+const styles = {
+  position: {
+    height: '100vh',
+  },
+  spacer: {
+    width: '100%',
+    height: '80px',
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div style={styles.position}>
+        <Navbar />
+        <div style={styles.spacer}>
+          &nbsp;
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <TableItem />
+        </Container>
       </div>
     );
   }
